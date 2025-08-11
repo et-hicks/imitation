a8b6e9db8a5c898e186e25d7e4b5c674eb296544
Ethan Hicks <ehicks@berkeley.edu>
2025-08-11 11:01:23 -0400

    fuck it we ball

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,6 @@
 export const BACKEND_URL = "https://go-example-bitter-cherry-6166.fly.dev"
+export const NEXT_PUBLIC_SUPABASE_URL="https://kaodsutlbtjiffsvmckx.supabase.co"
+export const NEXT_PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imthb2RzdXRsYnRqaWZmc3ZtY2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NjExNzQsImV4cCI6MjA3MDMzNzE3NH0.v0hDI8nT54mEUbdqzhlVfdgIeJXcjWq-SAyPuP87jNc"
 
 if (!BACKEND_URL) {
   // eslint-disable-next-line no-console
