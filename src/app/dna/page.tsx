608e60503f95ae46443072d509ca7836f3e4563c
Ethan Hicks <ehicks@berkeley.edu>
2025-09-22 14:05:04 -0400

    setting up the dna page

---
+++ src/app/dna/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'DNA Project',
+};
+
+export default function DnaPage() {
+  return (
+    <main className="min-h-[calc(100vh-56px)] bg-black flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-white text-4xl font-semibold mb-6">
+          Welcome to DNA Polymerase Project
+        </h1>
+        <p className="text-white text-2xl">Ethan, Thalia, and Mubassera</p>
+      </div>
+    </main>
+  );
+}
